a769d27d6a58b702046842441f07189054666de3
Thiago Xavier <thiago.xavier@unidas.com.br>
2021-10-01 15:55:16 -0300

    teste

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringAzureDemoApplication {
 
-	@GetMapping("/message")
+	@GetMapping("/")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+		return "teste";
 	}
 
 	public static void main(String[] args) {
